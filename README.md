88c8b5ee79633903c551795163a41965576a90ab
Jacky Liang <coolboii@me.com>
2015-11-22 22:32:20 -0500

    Upgraded code

--- README.md
+++ README.md
@@ -1,47 +1,56 @@
 transmissionVPN
 ================
 
-This is a Transmission + VPN script for Synology NAS devices. Its entire purpose is to connect to a VPN of your choice (L2TP, PPTP, or OpenVPN) then checks the VPN interface's IP address and binds it to the Transmission configuration file. __Which means, Transmission ONLY connects to VPN while other applications do not!__
+This is a Transmission + VPN script for Synology NAS devices. Its entire purpose is to connect to a VPN of your choice (L2TP, PPTP, or OpenVPN) then checks the VPN interface's IP address and binds it to the Transmission configuration file. __In other words, Transmission ONLY connects to VPN while other applications DO NOT!__
 
 To use this script, you must first create the VPN using DiskStation Manager and install Transmission from SynoCommunity's repository. After all that, then change the variables in this script.
 
 ## Retrieve Variables
-By default, you must retrieve these bits of information before the script works. Here are the variables that may be changed depending on your configuration. The default settings will be contained inside the script. Please follow the exact format unless you know what you're doing.
+You must retrieve the required settings before the script works. Here are the variables that may be changed depending on your configuration. Examples will be provided inside the script. Please follow the exact format unless you know what you're doing. Some variables DO NOT need to be changed, but are provided in case you do. __If you need help, feel free to open an issue on GitHub!__
 
-__VPN_CONFID__ - Synology Configuration ID (Instructions Below)
+##### VPN Settings
+1. __VPN_CONFID__ - Synology Configuration ID (Instructions Below)
+2. __VPN_CONFNAME__ - Synology Configuration Name (l2tpclient, pptpclient, ovpnclient)
+3. __VPN_PROTO__ - Synology Protocol (l2tp, pptp, openvpn)
+4. __VPN_UINAME__ - Synology VPN Profile Name (Found in Control Panel > Network > Network Interface)
 
-__VPN_CONFNAME__ - Synology Configuration Name (l2tpclient, pptpclient, ovpnclient)
+##### VPN Optionals
+1. __VPN_TYPE__ - The type of connection you will route though (__ppp__ for L2TP/PPTP VPN, or __tun__ for OpenVPN)
+2. __VPN_INTERFACE__ - The default interface you will be connecting to (__ppp0__ for L2TP/PPTP, or __tun0__ for OpenVPN)
+3. __VPN_RETRY__ - How many retries (times) if VPN fails to connect (default is 10)
+4. __VPN_INTERVAL__ - The time (in seconds) to wait between each retry (default is 30)
 
-__VPN_PROTO__ - Synology Protocol (l2tp, pptp, openvpn)
-
-__VPN_UINAME__ - Synology VPN Profile Name (Found in Control Panel > Network > Network Interface)
-
-__TRANS_USER__ - The user running under Transmission
-
-__TRANS_GROUP__ - The group of Transmission's user
-
-__TRANS_VAR__ - Path where the settings.json file is located (Full Path, no ending backslash)
-
-__TRANS_SSSS__ - Path where the Transmission start-stop-status file is located (Full Path, no ending backslash)
+##### App Settings
+1. __TRANS_USER__ - The user running under Transmission
+2. __TRANS_GROUP__ - The group of Transmission's user
+3. __TRANS_VAR__ - Path where the settings.json file is located (Full Path, no ending backslash)
+4. __TRANS_SSSS__ - Path where the Transmission start-stop-status file is located (Full Path, no ending backslash)
 
 ## Get Synology Configuration ID
 To retrieve the __VPN_CONFID__, follow these steps:
 
-1. SSH into __/usr/syno/etc/synovpnclient/__
-2. Type cd YOUR_PROTOCOL (l2tp, openvpn, or pptp)
+1. SSH or Telnet into __/usr/syno/etc/synovpnclient/__
+2. Type cd PROTOCOL (Replace PROTOCOL with l2tp, openvpn, or pptp)
 3. Type in __ls -l__ to list out the files
-4. Find a file called __connect_l4758264759__ (similar example)
-5. __l4758264759__ (similar example) is your Configuration ID
+4. Find a file called __connect_l4758264759__
+5. __l1234567890__ is your Configuration ID
+6. Copy the text __l1234567890__ to the variable above
 
-NOTE: If you have more than one of the same protocol connections (e.g. Two L2TP VPNs), use __vim__ or __nano__ to dig around the files and find the correct Configuration ID.
+NOTE 1: __l1234567890__ is an example. Each configuration ID is unique and YOU must connect to your Synology NAS though SSH or Telnet to retrieve it.
+
+NOTE 2: If you have more than one of the same protocol connections (e.g. two L2TP VPNs), use __vim__ or __nano__ (install it with SynoCommunity's repo) to dig around the files and find the correct Configuration ID. __EXPERIENCED USERS ONLY!__
 
 ## Scheduling
-If you want to automate the script, you can use the Task Scheduler application provided inside the DiskStation Manager. Make sure the script is ran under root to prevent any issues.
+If you want to automate the script, you can use the Task Scheduler application provided inside the DiskStation Manager. Make sure the script is ran under root to prevent any issues occurring.
+
+## How to use
+Before you use this script, use the following commands:
+
+1. __transmissionvpn.sh install__ - INSTALLER. Stops VPN, binds 127.0.0.1 to Transmission
+2. __transmissionvpn.sh uninstall__ - UNINSTALLER. Stops VPN, binds 0.0.0.0 to Transmission
 
 To run this script, use the following commands:
 
-1. __transmissionvpn.sh start__
-2. __transmissionvpn.sh stop__
-
-## OpenVPN Fix
-This script by default detects and uses the ppp0 interface from the NAS. If you are using OpenVPN, please change the configuration to tun0 in the script. Future releases will contain a variable that makes changes easier.
+1. __transmissionvpn.sh start__ - Starts the VPN, binds VPN address to Transmission
+2. __transmissionvpn.sh stop__ - Stops the VPN, binds 127.0.0.1 to Transmission
+3. __transmissionvpn.sh autofix__ - Fixes stalled VPNs, decides which IP address to bind
--- transmissionVPN/transmissionvpn.sh
+++ transmissionVPN/transmissionvpn.sh
@@ -1,12 +1,19 @@
-# Transmission VPN for Synology NAS
-# ----------------------------------
+# transmissionVPN
+# ================
+
 # VPN Settings
 VPN_CONFID=l1234567890
 VPN_CONFNAME=l2tpclient
 VPN_PROTO=l2tp
-VPN_UINAME=VPN
+VPN_UINAME=YourVPN
 
-# System Settings
+# VPN Optionals
+VPN_TYPE=ppp
+VPN_INTERFACE=ppp0
+VPN_RETRY=10
+VPN_INTERVAL=30
+
+# App Settings
 TRANS_USER=transmission
 TRANS_GROUP=users
 TRANS_VAR=/volume1/@appstore/transmission/var
@@ -15,27 +22,37 @@ TRANS_SSSS=/var/packages/transmission/scripts/start-stop-status
 # Script Starts
 case "$1" in
 start)
-    if echo `ifconfig` | grep -q "ppp"; then
+    # Checks if VPN is connected
+    if echo `ifconfig` | grep -q "$VPN_TYPE"; then
+
+        # Show Message
         echo "VPN is connected!"
+
     else
+
+        # Show Message
         echo "VPN is connecting ..."
+
+        # Reconnect VPN connection
         echo conf_id=$VPN_CONFID > /usr/syno/etc/synovpnclient/vpnc_connecting
         echo conf_name=$VPN_CONFNAME >> /usr/syno/etc/synovpnclient/vpnc_connecting
         echo proto=$VPN_PROTO >> /usr/syno/etc/synovpnclient/vpnc_connecting
-        synovpnc reconnect --protocol=$VPN_PROTO --name=$VPN_UINAME --retry=10 --interval=30
+        synovpnc reconnect --protocol=$VPN_PROTO --name=$VPN_UINAME --retry=$VPN_RETRY --interval=$VPN_INTERVAL
 
-        if echo `ifconfig` | grep -q "ppp"; then
-            # Show Success Message
+        # Checks if VPN is connected
+        if echo `ifconfig` | grep -q "$VPN_TYPE"; then
+
+            # Show Message
             echo "VPN is connected!"
 
             # Get VPN IP Address
-            VPN_ADDR=`ifconfig ppp0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
-            echo "VPN Address is "$VPN_ADDR
+            VPN_ADDR=`ifconfig $VPN_INTERFACE | grep 'inet addr:' | cut -d: -f2 | awk '{print $1}'`
+            echo "VPN Address is: "$VPN_ADDR
 
             # Stops Transmission
             $TRANS_SSSS stop
 
-            # Binds IPv4 Address to ppp0
+            # Binds IPv4 VPN Address to interface
             echo "Binding IPv4 VPN Address ..."
             cat $TRANS_VAR/settings.json | sed "s/.*bind-address-ipv4.*/    \"bind-address-ipv4\"\: \"$VPN_ADDR\",/g" > $TRANS_VAR/settings.json.bak
             chmod 600 $TRANS_VAR/settings.json.bak
@@ -44,41 +61,188 @@ start)
 
             # Starts Transmission
             $TRANS_SSSS start
+
         else
+
+            # Show Message
             echo "ERROR 1000: VPN cannot be connected."
-            exit 1
+
         fi
+
     fi
 ;;
 stop)
-    if echo `ifconfig` | grep -q "ppp"; then
+    # Checks if VPN is connected
+    if echo `ifconfig` | grep -q "$VPN_TYPE"; then
+
+        # Stops Transmission
+        $TRANS_SSSS stop
+
+        # Binds IPv4 Home Address to interface
+        echo "Binding IPv4 Home Address ..."
+        cat $TRANS_VAR/settings.json | sed "s/.*bind-address-ipv4.*/    \"bind-address-ipv4\"\: \"127.0.0.1\",/g" > $TRANS_VAR/settings.json.bak
+        chmod 600 $TRANS_VAR/settings.json.bak
+        chown $TRANS_USER:$TRANS_GROUP $TRANS_VAR/settings.json.bak
+        mv $TRANS_VAR/settings.json.bak $TRANS_VAR/settings.json
+
+        # Starts Transmission
+        $TRANS_SSSS start
+
+        # Show Message
         echo "VPN is disconnecting ..."
+
+        # Kill VPN connection
         synovpnc kill_client
 
-        if echo `ifconfig` | grep -q "ppp"; then
-            echo "ERROR 1001: VPN cannot be disconnected."
-            exit 1
-        else
-            # Stops Transmission
-            $TRANS_SSSS stop
+        if echo `ifconfig` | grep -q "$VPN_TYPE"; then
 
-            # Binds IPv4 Address to ppp0
-            echo "Binding IPv4 Home Address ..."
-            cat $TRANS_VAR/settings.json | sed "s/.*bind-address-ipv4.*/    \"bind-address-ipv4\"\: \"127.0.0.1\",/g" > $TRANS_VAR/settings.json.bak
+            # Show Message
+            echo "ERROR 1001: VPN cannot be disconnected."
+
+        else
+
+            # Show Message
+            echo "VPN is disconnected!"
+
+        fi
+
+    else
+
+        # Show Message
+        echo "VPN is disconnected!"
+
+    fi
+;;
+repair)
+    # Define variables
+    VPN_ADDR=`ifconfig $VPN_INTERFACE | grep 'inet addr:' | cut -d: -f2 | awk '{print $1}'`
+    VPN_RESP=`curl -sS --interface $VPN_INTERFACE http://ipinfo.io/ip`
+
+    # If IP Address does not match
+    if [ "$VPN_ADDR" != "$VPN_RESP" ]; then
+
+        # Show Message
+        echo "VPN is not working ..."
+
+        # Stops Transmission
+        $TRANS_SSSS stop
+
+        # Binds IPv4 Home Address to interface
+        echo "Binding IPv4 Home Address ..."
+        cat $TRANS_VAR/settings.json | sed "s/.*bind-address-ipv4.*/    \"bind-address-ipv4\"\: \"127.0.0.1\",/g" > $TRANS_VAR/settings.json.bak
+        chmod 600 $TRANS_VAR/settings.json.bak
+        chown $TRANS_USER:$TRANS_GROUP $TRANS_VAR/settings.json.bak
+        mv $TRANS_VAR/settings.json.bak $TRANS_VAR/settings.json
+
+        # Checks if VPN is connected
+        if echo `ifconfig` | grep -q "$VPN_TYPE"; then
+
+            # Kill VPN connection
+            synovpnc kill_client
+
+        fi
+
+        # Reconnect VPN connection
+        echo conf_id=$VPN_CONFID > /usr/syno/etc/synovpnclient/vpnc_connecting
+        echo conf_name=$VPN_CONFNAME >> /usr/syno/etc/synovpnclient/vpnc_connecting
+        echo proto=$VPN_PROTO >> /usr/syno/etc/synovpnclient/vpnc_connecting
+        synovpnc reconnect --protocol=$VPN_PROTO --name=$VPN_UINAME --retry=$VPN_RETRY --interval=$VPN_INTERVAL
+
+        # Redefine variables
+        VPN_ADDR=`ifconfig $VPN_INTERFACE | grep 'inet addr:' | cut -d: -f2 | awk '{print $1}'`
+        VPN_RESP=`curl -sS --interface $VPN_INTERFACE http://ipinfo.io/ip`
+
+        # Checks VPN connection
+        if [ "$VPN_ADDR" != "$VPN_RESP" ]; then
+
+            # Show Message
+            echo "ERROR 1002: VPN does not provide an internet connection or ipinfo.io is not accessible."
+
+            # Kill VPN connection
+            synovpnc kill_client
+
+        else
+
+            # Binds IPv4 VPN Address to interface
+            echo "Binding IPv4 VPN Address ..."
+            cat $TRANS_VAR/settings.json | sed "s/.*bind-address-ipv4.*/    \"bind-address-ipv4\"\: \"$VPN_ADDR\",/g" > $TRANS_VAR/settings.json.bak
             chmod 600 $TRANS_VAR/settings.json.bak
             chown $TRANS_USER:$TRANS_GROUP $TRANS_VAR/settings.json.bak
             mv $TRANS_VAR/settings.json.bak $TRANS_VAR/settings.json
 
-            # Starts Transmission
-            $TRANS_SSSS start
         fi
+
+        # Starts Transmission
+        $TRANS_SSSS start
+
     else
-        echo "VPN is disconnected!"
+
+        # Show Message
+        echo "VPN is working!"
+
     fi
 ;;
+install)
+    # Show Message
+    echo "Installing ..."
+
+    # Stops Transmission
+    $TRANS_SSSS stop
+
+    # Binds IPv4 Home Address to interface
+    echo "Binding IPv4 Home Address ..."
+    cat $TRANS_VAR/settings.json | sed "s/.*bind-address-ipv4.*/    \"bind-address-ipv4\"\: \"127.0.0.1\",/g" > $TRANS_VAR/settings.json.bak
+    chmod 600 $TRANS_VAR/settings.json.bak
+    chown $TRANS_USER:$TRANS_GROUP $TRANS_VAR/settings.json.bak
+    mv $TRANS_VAR/settings.json.bak $TRANS_VAR/settings.json
+
+    # Starts Transmission
+    $TRANS_SSSS start
+
+    # Checks if VPN is connected
+    if echo `ifconfig` | grep -q "$VPN_TYPE"; then
+
+        # Kill VPN connection
+        synovpnc kill_client
+
+    fi
+
+    # Show Message
+    echo "Installed!"
+;;
+uninstall)
+    # Show Message
+    echo "Uninstalling ..."
+
+    # Stops Transmission
+    $TRANS_SSSS stop
+
+    # Binds IPv4 Home Address to interface
+    echo "Binding IPv4 Home Address ..."
+    cat $TRANS_VAR/settings.json | sed "s/.*bind-address-ipv4.*/    \"bind-address-ipv4\"\: \"0.0.0.0\",/g" > $TRANS_VAR/settings.json.bak
+    chmod 600 $TRANS_VAR/settings.json.bak
+    chown $TRANS_USER:$TRANS_GROUP $TRANS_VAR/settings.json.bak
+    mv $TRANS_VAR/settings.json.bak $TRANS_VAR/settings.json
+
+    # Starts Transmission
+    $TRANS_SSSS start
+
+    # Checks if VPN is connected
+    if echo `ifconfig` | grep -q "$VPN_TYPE"; then
+
+        # Kill VPN connection
+        synovpnc kill_client
+
+    fi
+
+    # Show Message
+    echo "Uninstalled!"
+;;
 *)
-    echo "TransmissionVPN Usage: {start|stop}"
-    exit 1
+    echo "Welcome to transmissionVPN!"
+    echo "Usage: start|stop|repair|install|uninstall"
+    echo "Example: transmissionvpn.sh start"
+;;
 esac
 
 exit 0
