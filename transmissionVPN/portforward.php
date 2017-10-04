3e0156dd3d21cfdf21c2ccd271d0eaba02e4b16e
Jacky Liang <coolboii@me.com>
2017-10-03 23:08:03 -0400

    Improved code

--- transmissionVPN/portforward.php
+++ transmissionVPN/portforward.php
@@ -1,16 +1,19 @@
-<?php /* Port Forwarding Check for transmissionVPN
-    ------------------------------------------------------- */
-    
-    $host = $argv[1];
-    $port = $argv[2];
-    
-    $connection = @fsockopen($host, $port, $errno, $errstr, 10);
-    
-    if(is_resource($connection)) {
-        echo 'open' . "\n";
-        fclose($connection);
-    } elseif(empty($port)) {
-        echo 'empty' . "\n";
-    } else {
-        echo 'closed' . "\n";
-    }
+<?php
+/**
+ * Port forwarding check.
+ *
+ * @since 1.0.0
+ */
+$host = $argv[1];
+$port = $argv[2];
+
+$connection = @fsockopen( $host, $port, $errno, $errstr, 10 );
+
+if ( is_resource( $connection ) ) {
+	echo 'open' . "\n";
+	fclose( $connection );
+} elseif ( empty( $port ) ) {
+	echo 'empty' . "\n";
+} else {
+	echo 'closed' . "\n";
+}
